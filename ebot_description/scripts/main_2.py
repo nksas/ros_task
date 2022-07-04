529fd93fc8c76353ca49ff9557e77ac77f359ee8
guptaPrabhav <prabhav24gupta@gmail.com>
2022-07-04 23:34:03 +0530

    lane detection complete, need to send velocity data for movement

--- ebot_description/scripts/main_2.py
+++ ebot_description/scripts/main_2.py
@@ -2,20 +2,60 @@
 
 # Import the necessary libraries
 from cv2 import Canny
+from matplotlib import image
 import rospy # Python library for ROS
 from sensor_msgs.msg import Image # Image is the message type
 from cv_bridge import CvBridge # Package to convert between ROS and OpenCV Images
 import cv2 # OpenCV library
 import numpy as np # Numpy Library
 import matplotlib.pyplot as plt # Importing the pyplot package from the matplotlib library
- 
+
+def make_coordinates(image,line_parameters):
+  slope, intercept = line_parameters
+  #y1 = image.shape[0]
+  #y2 = int(y1*(3/5))
+  y1 = 350
+  y2 = 150
+  x1 = int((y1 - intercept)/slope)
+  x2 = int((y2 - intercept)/slope)
+  return np.array([x1,y1,x2,y2])
+
+
+def average_slope_intercept(image, lines):
+  left_fit = []
+  right_fit = []
+  for line in lines:
+    x1,y1,x2,y2 = line.reshape(4)
+    parameters = np.polyfit((x1,x2), (y1,y2), 1)
+    slope = parameters[0]
+    intercept = parameters[1]
+    if slope < 0:
+      left_fit.append((slope, intercept))
+    else:
+      right_fit.append((slope,intercept))
+  left_fit_average = np.average(left_fit, axis=0)
+  right_fit_average = np.average(right_fit, axis=0)
+  left_line = make_coordinates(image,left_fit_average)
+  right_line = make_coordinates(image, right_fit_average)
+  return np.array([left_line,right_line])
+
+def display_lines(image,lines):
+  line_image = np.zeros_like(image)
+  if lines is not None:
+    for line in lines:
+      x1,y1,x2,y2 = line.reshape(4)
+      cv2.line(line_image, (x1,y1), (x2,y2), (0,0,255), 10)
+  return line_image
+
+
 def region_of_interest(image):
   height = image.shape[0]
   width = image.shape[1]
   square = np.array([[(0,280),(360,130),(width,280)]])
   mask = np.zeros_like(image)
   cv2.fillPoly(mask, square, 255)
-  return mask
+  masked_image = cv2.bitwise_and(image, mask)
+  return masked_image
 
 
 def callback(data):
@@ -33,18 +73,27 @@ def callback(data):
   lane_image = np.copy(current_frame)
 
   # Converting image to grayscale
-  gray = cv2.cvtColor(lane_image, cv2.COLOR_BGR2GRAY)
+  gray = cv2.cvtColor(current_frame, cv2.COLOR_BGR2GRAY)
 
   # Applying a Gaussian Blur
   blur = cv2.GaussianBlur(gray, (5, 5), 0)
 
   # Edge Detection using the Canny Edge Detector
-  canny = cv2.Canny(blur, 100, 200)
+  canny_image = cv2.Canny(blur, 100, 200)
 
-
+  # Cropped image
+  cropped_image = region_of_interest(canny_image)
   
+  lines = cv2.HoughLinesP(cropped_image, 2, np.pi/180, 100, np.array([]), minLineLength=30, maxLineGap=15)
+
+  averaged_lines = average_slope_intercept(lane_image, lines)
+
+  line_image = display_lines(lane_image, averaged_lines)
+
+  combo_image = cv2.addWeighted(lane_image, 0.8, line_image, 1, 1)
+
   # Display image using cv2
-  cv2.imshow("camera", region_of_interest(canny))
+  cv2.imshow("camera", combo_image)
    
   cv2.waitKey(1)
 
